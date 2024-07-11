59e6ba4043882e2a10c669c9c2b200e7782135c7
mitragroups <miftahmuhajir@gmail.com>
2024-07-11 18:12:13 +0700

    Add Header & Footer

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,12 @@
-import Image from "next/image";
+import { Header } from "@/components/header";
+import { Footer } from "@/components/footer";
 
 export default function Home() {
-  return <div>Hello World</div>;
+  return (
+    <div>
+      <Header />
+      <main>Hello World</main>
+      <Footer />
+    </div>
+  );
 }
