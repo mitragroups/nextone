ea0027785d9bed3b5372326bec67ac87e90a0422
mitragroups <miftahmuhajir@gmail.com>
2024-07-18 22:02:10 +0700

    Add header & footer in About Page

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,3 +1,12 @@
+import { Footer } from "@/components/footer";
+import { Header } from "@/components/header";
+
 export default function About() {
-  return <div>About</div>;
+  return (
+    <div>
+      <Header />
+      <main>About</main>
+      <Footer />
+    </div>
+  );
 }
