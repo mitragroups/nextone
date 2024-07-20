4fd11a4a1d3d508d434230e69e725891984f60aa
mitragroups <miftahmuhajir@gmail.com>
2024-07-20 18:16:23 +0700

    Add Prons Card

---
+++ src/components/card.jsx
@@ -0,0 +1,9 @@
+export const Card = ({ name, age, gender }) => {
+  return (
+    <main>
+      <div>Name: {name}</div>
+      <div>Age: {age}</div>
+      <div>Gender: {gender}</div>
+    </main>
+  );
+};
